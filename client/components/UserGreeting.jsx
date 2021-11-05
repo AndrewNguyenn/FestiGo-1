63a772b32b6f8f246a050ce16bd3f5a12a1cb8e2
Andrew Nguyen <stageandrewnguyen@gmail.com>
2021-11-05 19:49:06 -0400

    if commited, this will render 3 buttons on the page

---
+++ client/components/UserGreeting.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const UserGreeting = () => {
+  return (
+    <div>
+      <h1>Welcome back, {user}!</h1>
+      <h5>What would we like to do today?</h5>
+    </div>
+  );
+}
