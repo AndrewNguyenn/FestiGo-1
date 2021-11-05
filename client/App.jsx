4773ac65665db1a0fc5be3f5d7581325c13f503b
han807 <hanjo80714@gmail.com>
2021-11-05 14:25:19 -0700

    clean up

--- client/App.jsx
+++ client/App.jsx
@@ -6,27 +6,11 @@ import {
     useParams,
     Switch
 } from "react-router-dom";
-import Nav from "./components/Nav.jsx";
-import Home from "./components/Home.jsx";
-import TopicsDisplay from "./components/TopicsDisplay.jsx";
-import SpecTopicDisplay from "./components/SpecTopicDisplay.jsx"
-
-
 import "./scss/styles.scss";
 
 const App = props => {
     return (
-        <Router>
-            <Nav/>
-            <Switch>
-                <Route exact path="/"
-                    component={Home}/>
-                <Route exact path="/topics/:topicId"
-                    component={TopicsDisplay}/>
-                <Route exact path="/topics/:topicId/:topicName"
-                    component={SpecTopicDisplay}/>
-            </Switch>
-        </Router>
+        <div></div>
     );
 };
 
