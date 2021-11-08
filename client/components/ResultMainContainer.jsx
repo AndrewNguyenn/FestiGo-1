805dfecdf02594e986fbc563dedc63313dea2810
Michael Chin <mikechin37@gmail.com>
2021-11-08 14:36:04 -0500

    fixed up styling terence and michael

--- client/components/ResultMainContainer.jsx
+++ client/components/ResultMainContainer.jsx
@@ -10,52 +10,51 @@ import { render } from 'react-dom';
 
 
 const ResultMainContainer = props =>{
-  const {img, title, eventInfo, eventDescription, eventLocation} = props.eventObj;
+  const {image_url, name, start_date, start_time, venue, city, tm_url, country} = props.eventObj;
 
       return(
         <div className="result-container">
             {/* <ResultImageDisplay/> */}
           <div className="eventImageBox" id="eventImage">
             <div className="innerbox">
-              <label htmlFor="eventImage">Image here: </label>
               <p>
-                <span id="eventImage">{img}</span>
+                <span id="eventImageSpan"><img className="eventImage" src={image_url} /></span>
               </p>
             </div>
           </div>
             {/* <ResultTitleDisplay/> */}
           <div className="eventTitleBox" id="eventTitle">
             <div className="innerbox">
-              <label htmlFor="eventTitle">Title box here: </label>
+              <label htmlFor="eventTitle"></label>
               <p>
-                <span id="eventTitle">{title}</span>
+                <span id="eventTitle">{name}</span>
               </p>
             </div>
           </div>
             {/* <ResultEventDisplay/> */}
           <div className="eventInfoBox" id="eventInfo">
             <div className="innerbox">
-              <label htmlFor="eventInfo">EventInfo here: </label>
+              <label htmlFor="eventInfo"></label>
               <p>
-                <span id="eventInfo">{eventInfo}</span>
+                <span id="eventInfo">Date: {start_date}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Start Time: {start_time} </span>
               </p>
             </div>
           </div>
         {/* <ResultDescriptionDisplay/> */}
           <div className="eventDescriptionBox" id="eventDescription">
             <div className="innerbox">
-              <label htmlFor="eventDescription">Event Description here: </label>
+              <label htmlFor="eventDescription"></label>
               <p>
-                <span id="eventDescription">{eventDescription}</span>
+                <span id="eventDescription"><a href={tm_url}>Here</a>&nbsp;for More Information and to Buy Tickets</span>
               </p>
             </div>
           </div>
         {/* ResultLocationDisplay */}
             <div className="eventLocationBox" id="eventLocation">
               <div className="innerbox">
-                <label htmlFor="eventLocation">Location box here: </label>
+                <label htmlFor="eventLocation">Location: </label>
                 <p>
-                  <span id="eventLocation">{eventLocation}</span>
+                  <span id="eventLocation">{venue}, {city}, {country}</span>
                 </p>
               </div>
             </div>
